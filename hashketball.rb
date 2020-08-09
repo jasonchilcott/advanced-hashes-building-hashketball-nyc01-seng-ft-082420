df59c6473afe3ecfef9068874eb005d9feb714f3
Jason Chilcott <jasonchilcott@me.com>
2020-08-09 23:22:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,19 +4,19 @@ require 'pry'
 def game_hash
   hashketball = {
     home: {
-      team_name: "",
-      colors: ["", ""],
+      team_name: "Brooklyn Nets",
+      colors: ["Black", "White"],
       players: [
         {
-          player_name: "",
+          player_name: "Alan Anderson",
           number: 0,
-          shoe: 0,
-          points: 0,
-          rebounds: 0,
-          assists: 0,
-          steals: 0,
-          blocks: 0,
-          slam_dunks: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 1,
         }
 
       ]
